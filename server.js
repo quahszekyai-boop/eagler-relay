4ca7936f600a6a3f15c173f5b617ebfc3b3ed03d
quahszekyai-boop <quah_sze_kyai@students.edu.sg>
2026-09-19 17:38:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const http = require("http");
 
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Relay server is running!");
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Listening on port ${port}`);
+});
